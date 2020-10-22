232ba708e80b3eb369b873e7dacc75d6c4d2cac6
Narendra <69212699+narendra-gowda@users.noreply.github.com>
2020-10-22 17:47:46 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import React from 'react';
 function App() {
   return (
     <div>
-      Phone Directory
+      <div className="header">
+        Phone Directory
+    </div>
+      <div>
+        <button>Add</button>
+      </div>
+      <div>
+        <span>Name</span><br />
+        <span>Phone</span>
+      </div>
     </div>
   );
 }
