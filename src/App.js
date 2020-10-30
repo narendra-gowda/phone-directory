244d75e493721806d44ac7aff42004238d2176ea
Narendra <69212699+narendra-gowda@users.noreply.github.com>
2020-10-30 11:07:34 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -5,15 +5,40 @@ import './App.css';
 
 
 function App() {
+  let subscribers = [
+    { id: 1,
+      name: 'Naren',
+     phone: 9980636388
+    },
+    { id: 2,
+      name: 'Deepu',
+      phone: 1234567890
+    },
+    {
+      id:3,
+      name: 'Jinny',
+      phone: 8125637264
+    }
+  ]
   return (
     <Fragment>
       <Header/>
       <div className="body-container">
         <button className="add-btn">ADD</button>
-        <div id="fields-section">
-          <span className="fields">NAME</span>
-          <span className="fields">PHONE</span>
+        <div className="grid-container">
+          <span className="grid-headings">NAME</span>
+          <span className="grid-headings">PHONE</span>
         </div>
+        {
+          subscribers.map(sub =>{
+            return(
+              <div className="grid-container" key={sub.id}>
+                <span className="grid-items">{sub.name}</span>
+                <span className="grid-items">{sub.phone}</span>
+              </div>
+            )
+          })
+        }
       </div>
     </Fragment>
   );
